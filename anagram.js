aa3b852007e1ad2b91ed7812a497455d33ef360b
Jim Ezesinachi <ezesinachijim@gmail.com>
2021-11-25 12:12:45 +0100

    minor bug fix

--- anagram.js
+++ anagram.js
@@ -2,8 +2,8 @@ function isAnagram(str1, str2) {
     return formatStr(str1) === formatStr(str2);
 }
 
-// Helper function
 
+// Helper function
 function formatStr(str) {
     return str.replace(/[^\w]/g, '')
     .toLowerCase()
